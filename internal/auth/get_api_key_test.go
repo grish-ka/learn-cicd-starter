ffbb6922f4ba5f5295417b7e8e81f86b5ba92873
grish-ka <grishka.krot@gmail.com>
2026-07-18 20:33:30 +0100

    add formating system checks

--- internal/auth/get_api_key_test.go
+++ internal/auth/get_api_key_test.go
@@ -6,7 +6,7 @@ import (
 
 func TestNoHeader(t *testing.T) {
 	ApiKey, err := GetAPIKey(nil)
-	if err != ErrNoAuthHeaderIncluded {	
+	if err != ErrNoAuthHeaderIncluded {
 		t.Errorf("Expected ErrNoAuthHeaderIncluded, got %v", err)
 	}
 	t.Logf("ApiKey: %s, Error: %v\n", ApiKey, err)
